8700b2c71348bb609e7dc91a6c93af0ebcad5129
Apiquet <anthony.piquet2@gmail.com>
2018-10-04 15:37:53 +0200

    Least Square GD and SGD

---
+++ project1/scripts/implementations.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""some helper functions for project 1."""
+import csv
+import numpy as np
+
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute loss, gradient
+        err = y - tx.dot(w)
+        grad = -tx.T.dot(err) / len(err)
+        loss = calculate_mse(err)
+        # gradient w by descent update
+        w = w - gamma * grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+        y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
+            # compute a stochastic gradient and loss
+            err = y - tx.dot(w)
+            grad = -tx.T.dot(err) / len(err)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+            # calculate loss
+            loss = compute_loss(y, tx, w)
+            # store w and loss
+            ws.append(w)
+            losses.append(loss)
+
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    return losses, ws
+
+
+def ridge_regression(y, tx, lambda ):
+    """Code"""
+    
+    return y
+
+
+def logistic_regression(y, tx, initial w, max iters, gamma):
+    """Code"""
+    
+    return y
+
+
+def reg_logistic_regression(y, tx, lambda, initial w, max iters, gamma):
+    """Code"""
+    
+    return y
