2eb1cacc9d6443f6f2e126aa16579117625efecd
ERBACHER Pierre <bot_@hotmail.fr>
2018-10-13 11:40:14 +0200

    add function
    Loss function
    MSE
    Batch iter
    Ridge regression

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -4,11 +4,36 @@ import csv
 import numpy as np
 
 
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+   
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+            
+            
+
+def calculate_mse(e):
+     return 1/2*np.mean(e**2)
+
+def compute_loss(y, tx, w):
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+def 
+    
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
     w = initial_w
     for n_iter in range(max_iters):
         # compute loss, gradient
@@ -16,22 +41,15 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
         grad = -tx.T.dot(err) / len(err)
         loss = calculate_mse(err)
         # gradient w by descent update
-        w = w - gamma * grad
-        # store w and loss
-        ws.append(w)
-        losses.append(loss)
+        w = w - gamma * grad     
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
-    return losses, ws
+    return loss, w
 
 
-def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-        y, tx, initial_w, batch_size, max_iters, gamma):
-    """Stochastic gradient descent."""
-    # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
+def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
+    """Stochastic gradient descent.""" 
     w = initial_w
     
     for n_iter in range(max_iters):
@@ -43,19 +61,19 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
             w = w - gamma * grad
             # calculate loss
             loss = compute_loss(y, tx, w)
-            # store w and loss
-            ws.append(w)
-            losses.append(loss)
 
         print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
-    return losses, ws
+    return loss, w
 
 
-def ridge_regression(y, tx, lambda ):
-    """Code"""
-    
-    return y
+def ridge_regression(y, tx, lamb ):
+    #L2 Regularization 
+    N = len(y)
+    lambprime = lamb * 2 * N
+    w = np.invert(np.transpose(tx).dot(tx) + lambprime*np.identity(N)) * np.transpose(tx).dot(y)
+    loss = compute_loss(y, tx, w)
+    return loss,w
 
 
 def logistic_regression(y, tx, initial w, max iters, gamma):
