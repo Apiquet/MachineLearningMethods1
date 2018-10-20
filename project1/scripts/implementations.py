96a95f18bf7cd53fe8caddd11ac78eed5e9c2e5a
ERBACHER Pierre <bot_@hotmail.fr>
2018-10-20 13:15:56 +0200

    Least_square

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -46,48 +46,43 @@ def compute_loss(y, tx, w):
     
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
-    w = initial_w
-    for i in range(len(w)-1):
-        b = w[:,i]
-        a = tx[i]
-        for n_iter in range(max_iters):
-            # compute loss, gradient
-            err = y[i] - a.dot(b)
-            grad = -a.T.dot(err)
-            loss = 1# calculate_mse(err)
-            # gradient w by descent update
-            w[:,i] = w[:,i] - gamma * grad     
-            
-
+   
+    w = initial_w[:]
+    for i in range(max_iters):
+        e = y - tx.dot(w)
+        grad = - tx.T.dot(e) / len(e)
+        w = w - gamma * grad
+    loss = calculate_mse(e)
     return loss, w
 
 
 def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
     """Stochastic gradient descent.""" 
-    w = initial_w
+    w = initial_w[:]
     
     for n_iter in range(max_iters):
         for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
             # compute a stochastic gradient and loss
             err = y - tx.dot(w)
+            
             grad = -tx.T.dot(err) / len(err)
             # update w through the stochastic gradient update
             w = w - gamma * grad
             # calculate loss
-            loss = compute_loss(y, tx, w)
+            
 
-        #print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-         #     bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        loss = compute_loss(y, tx, w)
+    return loss, w
+
+def least_squares(y, tx):
+   
+    a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a,b)
+    loss = compute_loss(y, tx, w)
     return loss, w
 
 
-def ridge_regression(y, tx, lamb ):
-    #L2 Regularization 
-    N = len(y)
-    lambprime = lamb * 2 * N
-    w = np.invert(np.transpose(tx).dot(tx) + lambprime*np.identity(N)) * np.transpose(tx).dot(y)
-    loss = compute_loss(y, tx, w)
-    return loss,w
 
 
 
@@ -96,9 +91,15 @@ def ridge_regression(y, tx, lamb ):
 y,x,i = load_csv_data('data/train.csv',sub_sample=False)
 
 #next step standardize X
-x, mean_x, std_x = standardize(x)
-initial_w = np.random.rand(x.shape[1],x.shape[0])
-loss, w = least_squares_GD(y,x,initial_w,10,0.95)
+x, mean_x, std_x = standardize(x) 
+#Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
+b = np.ones((x.shape[0],1), dtype = int)
+x = np.column_stack((b, x)) 
+initial_w = np.random.rand(x.shape[1],1)
+y = y.reshape(y.shape[0],1)
+#loss, w = least_squares_GD(y,x,initial_w,1000,0.001)
+#loss , w = least_squares_SGD(y,x,initial_w,1,1000,0.001)
+loss, w = least_squares(y, x )
 print(loss,w)
 
 
