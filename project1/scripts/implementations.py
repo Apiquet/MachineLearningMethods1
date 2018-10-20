e4c44d06589c22c89b270730c542bb0cb484481c
Apiquet <anthony.piquet2@gmail.com>
2018-10-20 21:05:26 +0200

    Adding ridge_regression

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -83,7 +83,12 @@ def least_squares(y, tx):
     return loss, w
 
 
-
+def ridge_regression(y, tx, lambda_ ):
+   """implement ridge regression."""
+    aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    a = tx.T.dot(tx) + aI
+    b = tx.T.dot(y)
+    return np.linalg.solve(a, b)
 
 
 
