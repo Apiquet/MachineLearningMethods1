a23007347cd6b474abb16acbddc7a1f76c245e4e
danieljsmarda <danieljsmarda@gmail.com>
2018-10-23 22:57:23 +0200

    Implemented LogReg and RegLogReg

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -2,9 +2,8 @@
 """some helper functions for project 1."""
 import csv
 import numpy as np
-import proj1_helpers
-from proj1_helpers import load_csv_data
-import pandas as pd
+# Import all packages; packages listed explicitly for clarity.
+from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
 
 
 
@@ -103,12 +102,23 @@ def ridge_regression(y, tx, lambda_ ):
    return np.linalg.solve(a, b)
 
 
-            
+def logistic_regression(y,tx,initial_w,max_iters,gamma):
+    w = initial_w[:]
+    for i in range(max_iters):
+        grad = np.matmul(tx.T, sigmoid(tx.dot(w) - y))
+        w = w - gamma * grad
+    loss = grad
+    return loss, w
 
+def reg_logistic_regression(y,tx,initial_w,max_iters,gamma,lambda_):
+    w = initial_w[:]
+    for i in range(max_iters):
+        grad = np.matmul(tx.T, sigmoid(tx.dot(w) - y)) + (lambda_*w)
+        w = w - gamma * grad
+    loss = grad
+    return loss, w
 
-
-
-
+'''
 def logistic_regression(y,tx,initial_w,max_iters,gamma):
     w = initial_w[:]
     for i in range(max_iters):
@@ -128,7 +138,7 @@ def logistic_regression_sgd(y,tx,initial_w,batch_size,max_iters,gamma):
             w = w - gamma * grad
     loss = calculate_mse(e)
     return loss, w
-    
+'''
 
 
 
@@ -138,12 +148,12 @@ TRAINING
 
 y,x,i = load_csv_data('data/train.csv',sub_sample=False)
 
-#next step standardize X
+# next step standardize X
 x, mean_x, std_x = standardize(x) 
-#Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
+# Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
 b = np.ones((x.shape[0],1), dtype = int)
 x = np.column_stack((b, x)) 
-#Now creat vector Polynomial basis
+# Now creat vector Polynomial basis
 #x = build_poly(x,31 )
 initial_w = np.random.rand(x.shape[1],1)
 y = y.reshape(y.shape[0],1)
@@ -151,25 +161,22 @@ y = y.reshape(y.shape[0],1)
 #loss, w = least_squares_GD(y,x,initial_w,1000,0.001)
 #loss , w = least_squares_SGD(y,x,initial_w,1,500,0.001)
 #loss, w = least_squares(y, x)
-loss, w = logistic_regression(y,x,initial_w,1000,0.001)
+#loss, w = logistic_regression(y,x,initial_w,1000,0.01)
+loss, w = reg_logistic_regression(y,x,initial_w,1000,0.01,.1)
 print(loss,w)
 
 
 y,x,i = load_csv_data('data/train.csv',sub_sample=False)
 x, mean_x, std_x = standardize(x) 
-#Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
+# Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
 b = np.ones((x.shape[0],1), dtype = int)
 x = np.column_stack((b, x)) 
 
-y_test = proj1_helpers.predict_labels(w, x)
+y_test = predict_labels(w, x)
 n = 0
 for i in range(len(y)-1):
-    if (y[i] == y_test[i] ): 
-        n=n+1
+    if (y[i] == y_test[i]): 
+        n += 1
         
 print("Prediction accurracy: " )
 print(n/len(y))
-    
-
-
-
