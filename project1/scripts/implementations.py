16b7b7e630a7a4dc09075067ec412cbfcc8f1257
danieljsmarda <danieljsmarda@gmail.com>
2018-10-25 17:25:27 +0200

    Corrected Logistic Regression Function

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -9,7 +9,7 @@ from proj1_helpers import load_csv_data, predict_labels, create_csv_submission
 
 def sigmoid(t):
      return (1/(1+np.exp(-t)))
-    
+
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
     poly = np.ones((len(x), 1))
@@ -28,7 +28,7 @@ def standardize(x):
     return x, mean_x, std_x
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-   
+
     data_size = len(y)
 
     if shuffle:
@@ -43,8 +43,8 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
-            
-            
+
+
 
 def calculate_mse(e):
      return (1/2)*np.mean(e**2)
@@ -54,10 +54,10 @@ def compute_loss(y, tx, w):
     return calculate_mse(e)
 
 
-    
+
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
-   
+
     w = initial_w[:]
     for i in range(max_iters):
         e = y - tx.dot(w)
@@ -68,25 +68,25 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
-    """Stochastic gradient descent.""" 
+    """Stochastic gradient descent."""
     w = initial_w[:]
-    
+
     for n_iter in range(max_iters):
         for y_batch, tx_batch in batch_iter(y, tx, batch_size=batch_size, num_batches=1):
             # compute a stochastic gradient and loss
             err = y - tx.dot(w)
-            
+
             grad = -tx.T.dot(err) / len(err)
             # update w through the stochastic gradient update
             w = w - gamma * grad
             # calculate loss
-            
+
 
         loss = compute_loss(y, tx, w)
     return loss, w
 
 def least_squares(y, tx):
-   
+
     a = tx.T.dot(tx)
     b = tx.T.dot(y)
     w = np.linalg.solve(a,b)
@@ -105,7 +105,7 @@ def ridge_regression(y, tx, lambda_ ):
 def logistic_regression(y,tx,initial_w,max_iters,gamma):
     w = initial_w[:]
     for i in range(max_iters):
-        grad = np.matmul(tx.T, sigmoid(np.matmul(tx,w) - y))
+        grad = np.matmul(tx.T, sigmoid(np.matmul(tx,w)) - y)
         w = w - gamma * grad
     loss = grad
     return loss, w
@@ -113,7 +113,7 @@ def logistic_regression(y,tx,initial_w,max_iters,gamma):
 def reg_logistic_regression(y,tx,initial_w,max_iters,gamma,lambda_):
     w = initial_w[:]
     for i in range(max_iters):
-        grad = np.matmul(tx.T, sigmoid(np.matmul(tx,w) - y)) + (lambda_*w)
+        grad = np.matmul(tx.T, sigmoid(np.matmul(tx,w)) - y) + (lambda_*w)
         w = w - gamma * grad
     loss = grad
     return loss, w
@@ -127,7 +127,7 @@ def logistic_regression(y,tx,initial_w,max_iters,gamma):
         w = w - gamma * grad
     loss = calculate_mse(e)
     return loss, w
-    
+
 
 def logistic_regression_sgd(y,tx,initial_w,batch_size,max_iters,gamma):
     w = initial_w[:]
@@ -143,16 +143,16 @@ def logistic_regression_sgd(y,tx,initial_w,batch_size,max_iters,gamma):
 
 
 """
-TRAINING 
+TRAINING
 """
 
 y,x,i = load_csv_data('data/train.csv',sub_sample=False)
 
 # next step standardize X
-x, mean_x, std_x = standardize(x) 
+x, mean_x, std_x = standardize(x)
 # Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
 b = np.ones((x.shape[0],1), dtype = int)
-x = np.column_stack((b, x)) 
+x = np.column_stack((b, x))
 # Now creat vector Polynomial basis
 #x = build_poly(x,31 )
 initial_w = np.random.rand(x.shape[1],1)
@@ -167,16 +167,16 @@ print(loss,w)
 
 
 y,x,i = load_csv_data('data/train.csv',sub_sample=False)
-x, mean_x, std_x = standardize(x) 
+x, mean_x, std_x = standardize(x)
 # Add one dimenssion to X with only 1,beacause 1*W0+ x1*W1 + ...
 b = np.ones((x.shape[0],1), dtype = int)
-x = np.column_stack((b, x)) 
+x = np.column_stack((b, x))
 
 y_test = predict_labels(w, x)
 n = 0
 for i in range(len(y)-1):
-    if (y[i] == y_test[i]): 
+    if (y[i] == y_test[i]):
         n += 1
-        
+
 print("Prediction accurracy: " )
 print(n/len(y))
